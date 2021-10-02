d6786936812cc5494bec9865851e733daa5cf80a
Kevin Covelli <kevin.covelli@mail.utoronto.ca>
2021-10-02 17:48:18 -0400

    basic rigidbody character controller

---
+++ Balance is Key GameJam 2/Assets/Scripts/RigidbodyController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// Script to controll a player with a rigidbody component
+public class RigidbodyController : MonoBehaviour
+{
+    public float Acceleration = 0.2f; // net force increase per FixedUpdate call
+    public float TurnSpeed = 2f; // turn speed in radians/second
+
+    [Range(1,4)] // lets you restrict the player number in the unity editor
+    public int player = 1;
+
+    private string _hAxis;
+    private string _vAxis;
+    private Rigidbody rb;
+
+    private Vector3 _input = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        _hAxis = "p" + player + "Horizontal";
+        _vAxis = "p" + player + "Vertical";
+    }
+
+    void Update()
+    {
+        _input = Vector3.zero;
+        _input.x = Input.GetAxis(_hAxis);
+        _input.z = Input.GetAxis(_vAxis);
+        print(_input);
+    }
+
+    // FixedUpdate is not affected by framerate, it is called at a fixed time interval so may be called 0, 1, or more times per depending on the current framerate
+    void FixedUpdate()
+    {
+        rb.AddForce(_input * Acceleration, ForceMode.Force);
+    }
+}
