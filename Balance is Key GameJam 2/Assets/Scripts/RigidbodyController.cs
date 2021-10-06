414a2815b0290fcf8059818b64e0d0e9f2eeb4f4
Kevin Covelli <kevin.covelli@mail.utoronto.ca>
2021-10-06 03:35:29 -0400

    experimenting with different kinds of joints

--- Balance is Key GameJam 2/Assets/Scripts/RigidbodyController.cs
+++ Balance is Key GameJam 2/Assets/Scripts/RigidbodyController.cs
@@ -9,7 +9,7 @@ public class RigidbodyController : MonoBehaviour
     public float Acceleration = 50f; // net force increase per FixedUpdate call
     public float TurnSpeed = 2f; // turn speed in radians/second
 
-    [Range(1,4)] // lets you restrict the player number in the unity editor
+    [Range(1, 4)] // lets you restrict the player number in the unity editor
     public int player = 1;
 
     private string _hAxis;
@@ -18,20 +18,20 @@ public class RigidbodyController : MonoBehaviour
     private Rigidbody _platform;
     private LineRenderer[] _lines;
     private SpringJoint[] _springs;
-    
+
     private Vector3 _input = Vector3.zero;
 
     // Start is called before the first frame update
     void Start()
     {
+        _hAxis = "p" + player + "Horizontal";
+        _vAxis = "p" + player + "Vertical";
+        
         _rb = GetComponent<Rigidbody>();
         _springs = GetComponents<SpringJoint>();
         _lines = transform.parent.GetComponentsInChildren<LineRenderer>();
         print(_lines.Length);
         _platform = _springs[0].connectedBody;
-        
-        _hAxis = "p" + player + "Horizontal";
-        _vAxis = "p" + player + "Vertical";
     }
 
     void Update()
@@ -45,7 +45,7 @@ public class RigidbodyController : MonoBehaviour
         else if (player == 2)
             _input.y = Input.GetAxis("rightTrigger");
 
-    for (int i = 0; i < _lines.Length; i++)
+        for (int i = 0; i < _lines.Length; i++)
         {
             Vector3[] pos = {_springs[i].anchor, _springs[i].connectedAnchor};
             _lines[i].SetPositions(pos);
@@ -55,6 +55,7 @@ public class RigidbodyController : MonoBehaviour
     // FixedUpdate is not affected by framerate, it is called at a fixed time interval so may be called 0, 1, or more times per depending on the current framerate
     void FixedUpdate()
     {
-        _rb.AddForce(_input * Acceleration, ForceMode.Force); // adjust the net force vector every frame by adding a force in the direction of the user input
+        _rb.AddForce(_input * Acceleration,
+            ForceMode.Force); // adjust the net force vector every frame by adding a force in the direction of the user input
     }
 }
